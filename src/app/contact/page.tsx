7f137d344971aab61aa250de6c5399eaf71f85d4
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2024-02-27 16:41:23 +0900

    fix: Suspenseを追加

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React, { use } from 'react';
-import { useRouter, usePathname, useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
@@ -93,109 +94,111 @@ function App() {
   };
 
   return (
-    <div style={{ paddingTop: "5em" }}
-    >
-      <Navbar expand="lg" className="bg-body-tertiary" fixed="top" bg="light" variant="light" expanded={expanded} onToggle={setExpanded}
-        style={{
-          zIndex: 1000,
-          borderBottom: "2px solid #ffc4f2"
-        }}
+    <Suspense>
+      <div style={{ paddingTop: "5em" }}
       >
-        <Container>
-          <Navbar.Brand href="">だいたいBPM - お問い合わせ</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" onClick={handleNavClose}
-            >
-              <Nav.Item className="nav-item">
-                <Nav.Link onClick={() => {
-                  window.location.href = "/";
-                }
-                }
-                >TOP</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Container >
-      </Navbar >
-      <Container
-      >
-        {/* <Button variant="primary" href="/"
+        <Navbar expand="lg" className="bg-body-tertiary" fixed="top" bg="light" variant="light" expanded={expanded} onToggle={setExpanded}
+          style={{
+            zIndex: 1000,
+            borderBottom: "2px solid #ffc4f2"
+          }}
         >
-          TOPに戻る
-        </Button> */}
-        <Form>
-          <Form.Group className="mb-3" style={{ maxWidth: "300px" }}
+          <Container>
+            <Navbar.Brand href="">だいたいBPM - お問い合わせ</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto" onClick={handleNavClose}
+              >
+                <Nav.Item className="nav-item">
+                  <Nav.Link onClick={() => {
+                    window.location.href = "/";
+                  }
+                  }
+                  >TOP</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container >
+        </Navbar >
+        <Container
+        >
+          {/* <Button variant="primary" href="/"
           >
-            <Form.Label>お問い合わせ内容を選択してください。</Form.Label>
-            <Form.Control as="select" onChange={e => {
-              setOptionValue(e.target.value);
-            }
-            }
-              value={optionValue}
-              disabled={isSending}
+            TOPに戻る
+          </Button> */}
+          <Form>
+            <Form.Group className="mb-3" style={{ maxWidth: "300px" }}
             >
-              <option disabled >選択してください</option>
-              <option value="1">情報の修正</option>
-              <option value="2">曲の追加</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group className="mb-3" style={{ maxWidth: "500px" }}
-          >
-            <Form.Label>本文を入力してください。&#040;300文字以内&#041;</Form.Label>
-            <Form.Control as="textarea" rows={10} onChange={e => {
-              setMainText(e.target.value);
-              setIsValidate(!/^\s*$/.test(e.target.value));
-            }}
-              value={mainText}
-              disabled={isSending}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit"
-            disabled={!isValidate || isSending}
-            onClick={(e) => {
-              e.preventDefault();
-              setIsSending(true);
-              setAlertMessagesFunc("送信しています...(しばらくしても完了しない場合は時間を開けて再度お願いいたします。)");
-              sendContactMailAPI(mainText, optionValue);
-            }}
-          >
-            送信
-          </Button>
+              <Form.Label>お問い合わせ内容を選択してください。</Form.Label>
+              <Form.Control as="select" onChange={e => {
+                setOptionValue(e.target.value);
+              }
+              }
+                value={optionValue}
+                disabled={isSending}
+              >
+                <option disabled >選択してください</option>
+                <option value="1">情報の修正</option>
+                <option value="2">曲の追加</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3" style={{ maxWidth: "500px" }}
+            >
+              <Form.Label>本文を入力してください。&#040;300文字以内&#041;</Form.Label>
+              <Form.Control as="textarea" rows={10} onChange={e => {
+                setMainText(e.target.value);
+                setIsValidate(!/^\s*$/.test(e.target.value));
+              }}
+                value={mainText}
+                disabled={isSending}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit"
+              disabled={!isValidate || isSending}
+              onClick={(e) => {
+                e.preventDefault();
+                setIsSending(true);
+                setAlertMessagesFunc("送信しています...(しばらくしても完了しない場合は時間を開けて再度お願いいたします。)");
+                sendContactMailAPI(mainText, optionValue);
+              }}
+            >
+              送信
+            </Button>
+            <br />
+            {isSending && (
+              <div className="spinner-border text-primary" role="status"
+              >
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
+            {/* {!isValidate && (
+              <span style={{ paddingLeft: "0.5em", color: "red" }}>内容を入力してください</span>
+            )} */}
+          </Form>
           <br />
-          {isSending && (
-            <div className="spinner-border text-primary" role="status"
-            >
-              <span className="sr-only">Loading...</span>
-            </div>
+          {alertMessages.length > 0 && (
+            alertMessages.map((item, index) => (
+              <div className='toast fade show' role='alert' aria-live='assertive' aria-atomic='true' key={index}>
+                <div className='toast-header'>
+                  <strong className='mr-auto'>メッセージ</strong>
+                  <small>今</small>
+                  <button type='button' className='ml-2 mb-1 close' data-dismiss='toast' aria-label='Close'
+                    onClick={() => {
+                      // 自分自身のidに対応するものを削除
+                      setAlertMessages(alertMessages => alertMessages.filter(alert => alert.id !== item.id));
+                    }}>
+                    <span aria-hidden='true'><X /> </span>
+                  </button>
+                </div>
+                <div className='toast-body'>
+                  {item.msg}
+                </div>
+              </div>
+            ))
           )}
-          {/* {!isValidate && (
-            <span style={{ paddingLeft: "0.5em", color: "red" }}>内容を入力してください</span>
-          )} */}
-        </Form>
-        <br />
-        {alertMessages.length > 0 && (
-          alertMessages.map((item, index) => (
-            <div className='toast fade show' role='alert' aria-live='assertive' aria-atomic='true' key={index}>
-              <div className='toast-header'>
-                <strong className='mr-auto'>メッセージ</strong>
-                <small>今</small>
-                <button type='button' className='ml-2 mb-1 close' data-dismiss='toast' aria-label='Close'
-                  onClick={() => {
-                    // 自分自身のidに対応するものを削除
-                    setAlertMessages(alertMessages => alertMessages.filter(alert => alert.id !== item.id));
-                  }}>
-                  <span aria-hidden='true'><X /> </span>
-                </button>
-              </div>
-              <div className='toast-body'>
-                {item.msg}
-              </div>
-            </div>
-          ))
-        )}
-      </Container>
-    </div>
+        </Container>
+      </div>
+    <Suspense/>
   );
 }
 
